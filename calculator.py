8434ddc4ea87c0fa0a405429a70b0d4f2052b480
SheikhJawad <136932922+SheikhJawad@users.noreply.github.com>
2023-09-04 11:46:43 -0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,32 @@
+print("Choose an operation")
+print("1.Add")
+print("2.subtract")
+print("3.Multiplication")
+print("4.division")
+print("5.PowerMultiply")
+print("6.Remainder")
+operation = input ("enter your choice\n")
+if operation == "1":
+    num1 = input("enter your fisrt number\n")
+    num2 = input("enter your second number\n")
+    print("The result is ",int(num1)+int(num2))
+if operation== "2":
+       num1 = input("enter your fisrt number\n")
+       num2 = input("enter your second number\n")
+       print("The result is ",int(num1)-int(num2))
+if operation== "3":
+       num1 = input("enter your fisrt number\n")
+       num2 = input("enter your second number\n")
+       print("The result is ",int(num1)*int(num2))
+if operation== "2":
+       num1 = input("enter your fisrt number\n")
+       num2 = input("enter your second number\n")
+       print("The result is ",int(num1)/int(num2))
+if operation== "5":
+       num1 = input("enter your fisrt number\n")
+       num2 = input("enter your second number\n")
+       print("The result is ",int(num1)**int(num2))
+if operation== "6":
+       num1 = input("enter your fisrt number\n")
+       num2 = input("enter your second number\n")
+       print("The result is ",int(num1)%int(num2))      
